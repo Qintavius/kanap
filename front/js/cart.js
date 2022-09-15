b3d803eaa97266ab0f233caaf5fc5e46eb43de05
Qintavius <qtzaba@gmail.com>
2022-09-15 14:13:05 +0200

    [UPDATE] Refactorisation des fonctions affichage quantité total et prix total

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,108 +2,71 @@ let cart = JSON.parse(localStorage.getItem('cart'));
 // console.log(cart);
 
 let items = [];
-
-for(item of cart) {
-  document.querySelector('#cart__items').innerHTML += `
-      <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-          <div class="cart__item__img">
-            <img src="${item.image}" alt="${item.imageTxt}">
-          </div>
-          <div class="cart__item__content">
-            <div class="cart__item__content__description">
-              <h2>${item.name}</h2>
-              <p>${item.color}</p>
-              <p>${item.price} €</p>
+//-------------------------------------------------
+// Fonction affichage produit
+//-------------------------------------------------
+function displayItem() {
+  for(item of cart) {
+    document.querySelector('#cart__items').innerHTML += `
+        <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
+            <div class="cart__item__img">
+              <img src="${item.image}" alt="${item.imageTxt}">
             </div>
-            <div class="cart__item__content__settings">
-              <div class="cart__item__content__settings__quantity">
-                <p>Qté : ${item.quantity}</p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+            <div class="cart__item__content">
+              <div class="cart__item__content__description">
+                <h2>${item.name}</h2>
+                <p>${item.color}</p>
+                <p>${item.price} €</p>
               </div>
-              <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
+              <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Qté : ${item.quantity}</p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
+                </div>
               </div>
             </div>
-          </div>
-      </article>`;
-
-  items.push(item.id);
-  // console.log(items);
+        </article>`;
+  
+    items.push(item.id);
+    // console.log(items);
+  }
 }
+displayItem();
 
 //-------------------------------------------------
 // Fonction total quantitée
 //-------------------------------------------------
-let itemQuantity = function() {
-  let foundQuantity = cart.map((i) => i.quantity);
-  const reducer = (accumulator, curr) => accumulator + curr;
-// console.log(foundQuantity);
-  let sumQuantity = foundQuantity.reduce(reducer);
-  // for(i =0; i< items.length; i++){
-  //   totalQuantity = Number(foundQuantity[i]) + Number(totalQuantity);
-  // }
-  // console.log(foundQuantity.reduce(reducer));
-  return sumQuantity;
+displayTotalQuantity = (item) => {
+  const totalQuantity = document.getElementById("totalQuantity")
+  const total = cart.reduce((total, item) => total + item.quantity, 0)
+  totalQuantity.textContent = total;
 }
+displayTotalQuantity();
 
 //-------------------------------------------------
 // Fonction total prix
 //-------------------------------------------------
-let itemPrice = function() {
-  let foundPrice = cart.map((i) => i.totalPrice);
-  const reducer = (accumulator, curr) => accumulator + curr;
-// console.log(foundPrice);
-  let sum = foundPrice.reduce(reducer);
-  // for (let i = 0; i < items.length; i++){
-  //   sum += Number(foundPrice[i]) * Number(foundQuantity[i]);
-  // }
-  // console.log(sum);
-  // console.log(foundPrice.reduce(reducer));
-  return sum;
+displayTotalPrice = () => {
+  const totalPrice = document.getElementById("totalPrice")
+  const total = cart.reduce((total, item) => total + item.price * item.quantity, 0)
+  totalPrice.textContent = total;
 }
-console.log(itemPrice());
-
+displayTotalPrice();
 //-------------------------------------------------
 // sauvegarde du nouveau panier
 //-------------------------------------------------
-let updateCart = () => {
+function updateCart() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 // console.log(cart);
 
-//-------------------------------------------------
-// Fonction somme prix + quantité
-//-------------------------------------------------
-function total() {
-  let priceSum = itemPrice();
-  document.getElementById("totalPrice").innerHTML = priceSum;
+let itemNumber = document.querySelector('input.itemQuantity');
+console.log(itemNumber);
 
-  localStorage.setItem('priceSum', JSON.stringify(priceSum));
-
-  let quantitySum = itemQuantity();
-  document.getElementById("totalQuantity").innerHTML = quantitySum;
-
-  updateCart();
-}
-total();
-
-let selectedQuantity = Array.from(document.querySelector('.itemQuantity'));
-let totalPriceCart = Array.from(document.querySelector('#totalPrice'));
-let totalQuantity = Array.from(document.querySelector('.cart__item__content__settings__quantity p'));
-
-selectedQuantity.forEach(function (quantity, i) {
-  quantity.addEvenlister("input", (e) => {
-    e.preventDefault();
-    let addItemPrice = quantity.value * cart[i].price;
-    console.log(quantity.value);
-
-    totalQuantity[i].innerHTML = "Qté: " + quantity.value;
-    cart[i].quantity = parseInt(quantity.value);
-
-    totalPriceCart[i].innerHTML = addItemPrice;
-    cart[i].totalPriceCart = addItemPrice;
-
-    total();
-  });
+itemNumber.addEventListener('input', (q) => { 
+  // console.log(itemNumber);
+  
 });
-
