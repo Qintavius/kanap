4e3b8732c394bc7afd0b47991f7ef086c911fd47
Qintavius <qtzaba@gmail.com>
2022-09-20 16:18:54 +0200

    [UPDATE] Regex du formulaire + comportement validation des champs

--- front/js/cart.js
+++ front/js/cart.js
@@ -102,7 +102,7 @@ for(let i = 0; i < deleteButton.length; i++) {
     event.preventDefault();
     console.log(deleteButton);
 
-  let productInStorage = cart[i].id && cart[i].color;
+  let productInStorage = cart[i].id && cart[i].color; // var de condition de suppression
   console.log('resultat ' + productInStorage);
 
   cart = cart.filter( el => el.id !== productInStorage && el.color !== productInStorage)
@@ -120,3 +120,63 @@ for(let i = 0; i < deleteButton.length; i++) {
 //-------------------------------------------------
 // Formulaire
 //-------------------------------------------------
+let submitForm = document.getElementById('order');
+
+// ***************** Validation ******************
+let firstName = document.getElementById('firstName');
+let errorFirstName = document.getElementById('firstNameErrorMsg');
+let firstNameValidation = /^[a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+)?/;
+
+let lastName = document.getElementById('lastName');
+let errorLastName = document.getElementById('lastNameErrorMsg');
+let lastNameValidation = /^[a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+)?/;
+
+let address = document.getElementById('lastName');
+let errorAddress = document.getElementById('addressErrorMsg');
+let adressValidation = /^[a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+)?/;
+
+let city = document.getElementById('city');
+let errorCity = document.getElementById('cityErrorMsg');
+let cityValidation = /^[a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÈÉÎÌ][a-zéèêàçîï]+)?/;
+
+
+submitForm.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if(firstName.validity.valueMissing) {
+    e.preventDefault();
+    errorFirstName.textContent = 'Veuillez renseigner ce champ';
+  } else if(firstNameValidation.test(firstName.value) == false) {
+    e.preventDefault();
+    errorFirstName.textContent = 'Format incorrect';
+  }
+
+  if(lastName.validity.valueMissing) {
+    e.preventDefault();
+    errorLastName.textContent = 'Veuillez renseigner ce champ';
+  } else if(lastNameValidation.test(lastName.value) == false) {
+    e.preventDefault();
+    errorLastName.textContent = 'Le format est incorrect';
+  }
+
+  if(address.validity.valueMissing) {
+    e.preventDefault();
+    errorAddress.textContent = 'Veuillez renseigner ce champ';
+  } else if(adressValidation.test(address.value) == false) {
+    e.preventDefault();
+    errorAddress.textContent = 'Format incorrect';
+  }
+
+  if(city.validity.valueMissing) {
+    e.preventDefault();
+    errorCity.textContent = 'Veuillez renseigner ce champ';
+  } else if(cityValidation.test(city.value) == false) {
+    e.preventDefault();
+    errorCity.textContent = 'Format incorrect';
+  }
+
+})
+
+
+// let inputs = document.querySelectorAll('input[type="text"]');
+// console.log("result: " + inputs);
